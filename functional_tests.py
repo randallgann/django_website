39dab3573293d3369a012d43d55cb9aaf07e3a01
Randall Gann <etexdigital@outlook.com>
2022-10-17 19:59:55 -0500

    First FT and now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,11 +1,51 @@
 
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
+import unittest
 
-# Create a new instance of the Firefox driver and navigate to the URL http://localhost:8000 and assert 'Django' in the title
+class NewVisitorTest(unittest.TestCase):
+    
+        def setUp(self):
+            options = Options()
+            options.binary_location = 'C:\\Program Files\\Mozilla Firefox\\firefox.exe'
+            self.browser = webdriver.Firefox(options=options)
+    
+        def tearDown(self):
+            self.browser.quit()
+    
+        def test_can_start_a_booking_(self):
 
-options = Options()
-options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
-browser  =  webdriver.Firefox(options=options)
-browser.get( 'http://localhost:8000' )
-assert  'Django'  in  browser.title
+            # user goes to the booking page
+            self.browser.get('http://localhost:8000')
+    
+            # She notices the page title and header mention alabaster trailer
+            self.assertIn('Alabaster Trailer', self.browser.title)
+            self.fail('Finish the test!')
+
+            # She is invited to choose a date and time to rent the trailer.
+
+
+            # She chooses a date and time and clicks the 'Rent' button.
+
+            # She is taken to a page that shows her the date and time she has chosen.
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
+
+  
+
+
+
+
+# She is also shown a confirmation number.
+
+# She is invited to enter her name and email address.
+
+# She enters her name and email address and clicks the 'Confirm' button.
+
+# She is taken to a page that shows her the date and time she has chosen, her name and email address, and the confirmation number.
+
+# A confirmation email is sent to her email address.
+
+# satisfied, she goes back to sleep
